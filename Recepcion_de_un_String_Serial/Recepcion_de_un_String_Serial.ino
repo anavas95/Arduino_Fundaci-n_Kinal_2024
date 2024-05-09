a6b2decbc5a74567d3e9396d4bb588a44abb4287
Alejandro Navas <39711703+anavas95@users.noreply.github.com>
2024-05-09 09:16:35 -0600

    Serial

---
+++ Recepcion_de_un_String_Serial/Recepcion_de_un_String_Serial.ino
@@ -0,0 +1,31 @@
+/*
+   Fundacion Kinal
+   Centro educativo tecnico laboral Kinal
+   Quinto perito
+   Quinto electronica
+   Codigo Tecnico: EB5AM / EB5AV 
+   Curso: Taller de electronica digital y reparacion de computadoras I
+   Proyecto: Recepción de una cadena de caracteres por medio del puerto serie.
+   Dev: Profe. Alejandro Lopez Navas
+   Fecha: 07 de mayo
+   link de la simulacion: 
+   
+*/
+
+void setup() {
+  Serial.begin(19200);      //Inicio la comunicacion serial a 19200 bps.
+  Serial.println("Clase de prueba - Envio y recepción de datos por medio de UART");
+
+}
+
+void loop() {
+
+  //mientras exista informacion disponible
+  while(Serial.available()>0)
+  {
+    String info_recibida = Serial.readStringUntil('\n');
+    Serial.println(info_recibida);
+  }
+  info_recibida = "";  //limpio el string
+  
+}
